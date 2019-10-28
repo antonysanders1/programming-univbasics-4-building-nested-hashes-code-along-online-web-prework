8ab9b4e62d79ea3a4cfdaad257a740ed5af49cc7
Antony Sanders <antony.devon92@gmail.com>
2019-10-28 13:22:16 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,8 @@ def third_challenge
         #your key/value pairs here
         name: "Romeo", age: "15", status: "alive"
         },
-      :hero_friends => [ {name: "Benvolio", age: "17", attitude: "worried"},
+      :hero_friends => [ 
+        {name: "Benvolio", age: "17", attitude: "worried"},
          {name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }, 
    :capulet => {
@@ -29,7 +30,9 @@ def third_challenge
         #your key/value pairs here
         name: "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [
+        {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}]
    }
   }
 
